64e2b84aff31c416bd3d70f78108c6507f992c5b
GoldenKazoo <125774398+GoldenKazoo@users.noreply.github.com>
2024-05-12 21:39:49 +0200

    commit test

---
+++ test.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class test {
+
+public static void main(String [] args)
+{
+    int size = 800;
+    JFrame fenetre = new JFrame("Blockade");
+    fenetre.setVisible(true);
+    fenetre.setSize(size,size);
+    fenetre.setLocationRelativeTo(null); //pour ouvrir au centre
+    fenetre.setResizable(false);
+    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
+}
