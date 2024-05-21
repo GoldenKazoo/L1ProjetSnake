f1ff173569dbd40b82d29cfaa95600097b810031
Made in China <102796291+FILATURQUE@users.noreply.github.com>
2024-05-21 11:54:36 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 /**
@@ -5,23 +7,24 @@ import javax.swing.*;
  */
 public class Joueur {
 
-    public static void coupInput(){ // Creation de la frame input coup 
-        JFrame frame = new JFrame("Input");
+
+    public static String creationFrame(String question){ // Creation de la frame input coup 
+        JFrame frame = new JFrame("Frame");
     
         JTextField text1 = new JTextField();
         text1.setBounds(20,60,200,28);
         
-        JLabel text2 = new JLabel("Quel direction voulez vous aller ?");
+        JLabel text2 = new JLabel(question);
         text2.setBounds(20,20,200,28);
 
-        JButton entrer = new JButton("Entrer");
-        entrer.setBounds(65, 100, 90, 28);
+        JButton button = new JButton("Entrer");
+        button.setBounds(65, 100, 90, 28);
         
 
         // Ajout des text, input
         frame.add(text1); 
         frame.add(text2);
-        frame.add(entrer);
+        frame.add(button);
 
         // Application des settings de la fenetre
         frame.setSize(250,180);
@@ -31,15 +34,18 @@ public class Joueur {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Recuperation de l'input 
-        String direction = text1.getText();
-        System.out.println(direction);
+        String textCoup = text1.getText();
+        
+        return textCoup;
+
     }
         public static void getText(){
+            String getText = creationFrame("Quel est votre coup?");
+            System.out.println(getText);
             
         }
 
     public static void main(String[] args) {
-        coupInput();
+        getText();
     }
 }
